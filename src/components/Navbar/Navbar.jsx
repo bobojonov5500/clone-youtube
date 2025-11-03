426c8e4aaa4ee3307b2e1f06a07f45bf4578b253
asad bobojonov <bobojonov4664304@gmail.com>
2025-11-03 17:47:52 +0500

    some improvments

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,13 @@
-import React from "react";
 import Search from "../Search/search-bar";
 import { Link } from "react-router-dom";
-import { TbBrandYoutubeKids } from "react-icons/tb";
+import { SiYoutubeshorts } from "react-icons/si";
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between p-2 sticky top-0 z-[99] w-full bg-green-100">
+    <div className="flex items-center px-4 justify-between p-2 sticky top-0 z-[99] w-full bg-green-100">
       {/* Logo */}
       <Link to="/home">
-        {/* <img
-          className="w-12 h-auto hover:bg-red-600"
-          src="https://www.svgrepo.com/show/79316/youtube.svg"
-          alt="logo"
-        /> */}
-        <TbBrandYoutubeKids
+        <SiYoutubeshorts
           className="w-[50px] h-auto transition duration-300 ease-in-out 
              hover:text-red-700 hover:-translate-y-1 hover:scale-110"
         />
